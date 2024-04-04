802e975291f837aaf65b2da512b80d94a12d7884
Shishkin PRO <viktorshish@gmail.com>
2024-04-04 22:08:41 +0300

    Unpacked the arguments

--- comics.py
+++ comics.py
@@ -43,16 +43,16 @@ def upload_image(url):
         files = {'photo': file}
         response = requests.post(url, files=files)
         response.raise_for_status()
-        return response.json()
+    return response.json()
 
 
-def save_image_in_albom(access_token, group_id, uploaded_image):
+def save_image_in_albom(access_token, group_id, photo, server_url, photo_hash):
     params = {
         'access_token': access_token,
         'group_id': group_id,
-        'photo': uploaded_image['photo'],
-        'server': uploaded_image['server'],
-        'hash': uploaded_image['hash'],
+        'photo': photo,
+        'server': server_url,
+        'hash': photo_hash,
         'v': '5.199',
     }
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
@@ -60,10 +60,7 @@ def save_image_in_albom(access_token, group_id, uploaded_image):
     return response.json()
 
 
-def publicate_image(access_token, group_id, saved_image, comics_alt):
-    image_owner_id = saved_image['owner_id']
-    image_media_id = saved_image['id']
-    image_url = saved_image['sizes'][-1]['url']
+def publicate_image(access_token, group_id, image_owner_id, image_media_id, image_url, comics_alt):
     image_url_netloc = urlparse(image_url).netloc
     image_url_path = urlparse(image_url).path
     image_url_params = urlparse(image_url).params
@@ -98,8 +95,16 @@ def main():
 
     upload_url = get_upload_vk_url(access_token, group_id)
     uploaded_image = upload_image(upload_url)
-    saved_image = save_image_in_albom(access_token, group_id, uploaded_image)['response'][-1]
-    publicate_image(access_token, group_id, saved_image, comics_alt)
+
+    photo = uploaded_image['photo']
+    server_url = uploaded_image['server']
+    photo_hash = uploaded_image['hash']
+    saved_image = save_image_in_albom(access_token, group_id, photo, server_url, photo_hash)['response'][-1]
+
+    image_owner_id = saved_image['owner_id']
+    image_media_id = saved_image['id']
+    image_url = saved_image['sizes'][-1]['url']
+    publicate_image(access_token, group_id, image_owner_id, image_media_id, image_url, comics_alt)
 
     os.remove('comics.png')
 
